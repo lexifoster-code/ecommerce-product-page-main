5c7a23278e723d137657c55d7f895c11e33c3c98
Alexis Foster <lexifoster.af@gmail.com>
2024-10-16 17:16:44 -0400

    css styles, lightbox feature

--- script.js
+++ script.js
@@ -26,3 +26,50 @@ decrement.addEventListener('click', ()=> {
 })
 
 updateDisplay();
+
+const lightbox = document.getElementById('lightbox');
+const closeButton = document.querySelector('.close');
+const mainImage = document.querySelector('.main-img');
+const lightboxImage = document.getElementById('lightbox-img');
+const thumbnails = document.querySelectorAll('.lightbox-side-img');
+
+ // Function to open the lightbox with the selected image
+ function openLightbox(imageSrc) {
+    lightbox.style.display = 'flex';
+    lightboxImage.src = imageSrc; // Set the main image
+}
+
+// Open lightbox with the main image
+mainImage.addEventListener('click', () => {
+    openLightbox(mainImage.src);
+});
+
+// Open lightbox with clicked thumbnail
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', (event) => {
+        const fullImageSrc = event.target.getAttribute('data-full-src'); // Get the full image source
+        openLightbox(fullImageSrc);
+    });
+});
+
+closeButton.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+// Close lightbox when clicking outside of the image
+lightbox.addEventListener('click', (event) => {
+    if (event.target === lightbox) {
+        lightbox.style.display = 'none';
+    }
+});
+
+const modal = document.getElementById('cartModal')
+
+const openModal = document.querySelector('.cart-icon');
+ openModal.addEventListener('click', () =>{
+   modal.style.display = "flex"
+ })
+ const closeModal = document.getElementById('closeModal');
+ closeModal.addEventListener('click', () =>{
+   modal.style.display = "none"
+ })
